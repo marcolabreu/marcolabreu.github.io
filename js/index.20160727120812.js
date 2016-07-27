620e548d86c053b309ebaf34ba13996a000c6b9b
Marco L. Abreu <marcolabreu@royalmail.top>
2016-07-27 12:55:11 +0200

    Add 3 more devices to responsive design
    The number of designs is now 5, with 3 landscape devices (1200, 960 and 480 pixels width) and 2 portrait devices (768 and 320 pixels height).

--- js/index.20160727120812.js
+++ js/index.20160727120812.js
@@ -1,14 +1,14 @@
 (function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
 $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
 
-var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=1200){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=480){cw++;}if(w>=768){cw++;}if(w>=960){cw++;}if(w>=1200){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
 function em1(){var c="nbsdpbouupoojpAhnbjm/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);location.href=addr;}
 
 $(document).ready(function() {
 if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
-$('.js-13').attr('src', 'images/flickr-white-logo-40.png');
-$('.js-14').attr('src', 'images/linkedin-white-logo-40.png');
-$('.js-15').attr('src', 'images/mail-white-logo-40.png');
+$('.js-31').attr('src', 'images/flickr-white-logo-40.png');
+$('.js-32').attr('src', 'images/linkedin-white-logo-40.png');
+$('.js-33').attr('src', 'images/mail-white-logo-40.png');
 
 }
 var wl = new woolite();
